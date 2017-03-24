47a22b091c5b8e9220de6bc01214d732c5354ae4
bryanhmiller <bryankalos@hotmail.com>
2017-03-23 20:35:49 -0500

    Completed objectives for seasonal sales assignment.  Code is working

--- seasonal-sales.js
+++ seasonal-sales.js
@@ -1,7 +1,7 @@
-// Your job is to build a web page that lists all of the products, the name of 
-// the department it's in, and the price. Additionally, put a <select> element 
+// *Your job is to build a web page that lists all of the products*, *the name of 
+// the department it's in*, *and the price.* Additionally, *put a <select> element 
 // at the top of the page that contains all possible values of the season_discount 
-// key in the categories file. As soon as you select one of the seasons, all prices 
+// key in the categories file.??* As soon as you select one of the seasons, all prices 
 // on the page should immediately be discounted by the corresponding percentage.
 
 // For example, when Spring is chosen, all products in the corresponding Household 
@@ -11,41 +11,62 @@
 // categories.json. You should load both file via XHRs and store the contents in 
 // two different JavaScript variables in your code.
 var categoryContainer = document.getElementById("categories");
-var catFoodBowl = document.getElementById("catFoodContainer");
+var productContainer = document.getElementById("products");
+var categories = [];
+var products = [];
+var dropdownSelect = document.getElementById("dropdown");
 
-function makeCategoryDom(xhrData){
-	var categoryString = "";
-	var currentCategory;
-	for (var i = 0; i < xhrData.categories.length; i++) {
-		currentCategory = xhrData.categories[i];
-	
-		categoryString += `<div id="${currentCategory.id}">This div has an id</div>`;
-		categoryString += `<div>${currentCategory.name}</div>`;
-		categoryString += `<div>${currentCategory.season_discount}</div>`;
-		categoryString += `<div>${currentCategory.discount}</div>`;
-
-		// for (var j = 0; j < currentCategory.types.length; j++) {
-		// 	categoryString += `<p>${currentCategory.types[j].type}</p>`;
-		// 	console.log(currentCategory.types[j].type);
-		// 	console.log(currentCategory.types[j].volumes);
-		// 	for (var k = 0; k < currentCategory.types[j].volumes.length; k++) {
-		// 		categoryString += `<p>Size: ${currentCategory.types[j].volumes[k].name}`;
-		// 		categoryString += `<p>$${currentCategory.types[j].volumes[k].price}`;
-		// 	}
-		// }
+function makeProductDom(discountSeason){
+	var productString = "";
+	var currentProduct;
+	for (var j = 0; j < products.length; j++) {
+		currentProduct = products[j];
+		productString += `<section class="col-sm-3 col-md-2 thumbnail">`;	
+		productString += `<div id="${currentProduct.name}">${currentProduct.name}</div>`;
+		productString += `<div id="${currentProduct.category_name}">${currentProduct.category_name}</div>`;
+		if (discountSeason === currentProduct.category_season_discount) {
+			productString += `<div class="price">${currentProduct.season_price}</div>`;
+		} else {
+			productString += `<div class="price">${currentProduct.price}</div>`;
+		}
+		productString += `</section>`;
 	}	
-	categoryContainer.innerHTML = categoryString;
+	productContainer.innerHTML = productString;
 }
 
 function categoriesMakerLoaded(){
 	var categoryData = JSON.parse(this.responseText);
-	makeCategoryDom(categoryData);
+	department = categoryData.categories
+}
+
+function productMakerLoaded(){
+	var productData = JSON.parse(this.responseText);
+	products = productData.products
+	products.forEach (function(product){
+		for (var i = 0; i < department.length; i++) {
+			if (product.category_id === department[i].id) {
+				product["category_name"] = department[i].name;
+				product["category_discount"] = department[i].discount;
+				product["category_season_discount"] = department[i].season_discount;
+				product["season_price"] = (product.price - (product.price * department[i].discount)).toFixed(2);
+			}
+		}
+	});
+	makeProductDom("none");
 }
 
 function categoriesFail(){
 	console.log("booooo");
 }
 
+function productFail(){
+	console.log("booooo");
+}
+
+// function selection() {
+// 	console.log(event);
+// 	console.log(event.target);
+// }
 
 var categoriesRequest = new XMLHttpRequest();
 categoriesRequest.addEventListener("load", categoriesMakerLoaded);
@@ -53,7 +74,12 @@ categoriesRequest.addEventListener("error", categoriesFail);
 categoriesRequest.open("GET", "categories.json");
 categoriesRequest.send();
 
-// Extranious Info
-// You work as a developer for a big box store. Here is a JSON representation of a 
-// small sample of the products you sell.
-// Here is a JSON representation of some of the departments in your stores.
+var productRequest = new XMLHttpRequest();
+productRequest.addEventListener("load", productMakerLoaded);
+productRequest.addEventListener("error", productFail);
+productRequest.open("GET", "products.json");
+productRequest.send();
+
+dropdownSelect.addEventListener("change", function(event){
+	makeProductDom(event.target.value);
+});
